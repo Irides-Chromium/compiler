00c231a4ca05c9ba6e80bc2dbb806eeaaa3c020a
Irides-Chromium <zhuhaoqing@live.cn>
2016-06-10 22:06:37 +0800

    back up

--- simple-symbol/ss.py
+++ simple-symbol/ss.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+TP_S = 30000
 
-MX = [[0 for i in range(30000)] for i in range(8)]     # Array for tapes
+MX = [[0 for i in range(TP_S)] for i in range(8)]     # Array for tapes
 TPS = [0, 0]                             # Tape Pointer Stack
 #      ^  ^
 #old_tape new tape (current)
@@ -10,12 +11,14 @@ def get_val(index=-1):
     TP = TPS[1]
     RP = RPS[TP]
     index = (index + 1 or RP + 1) - 1
+    # value = (32768 + value) % 65536 - 32768   # to simulate a `short int`
     return MX[TP][index]
 
 def set_val(value, index=-1):
     TP = TPS[1]
     RP = RPS[TP]
     index = (index + 1 or RP + 1) - 1
+    # value = (32768 + value) % 65536 - 32768   # to simulate a `short int`
     MX[TP][index] = value
 
 def set_TP(tape_num):
@@ -23,33 +26,48 @@ def set_TP(tape_num):
     TPS.pop(0)
 
 def set_RP(index):
+    if index < 0: sys.exit("error: tape memory out of bounds (underrun)\n"\
+                    "undershot the tape size of %d cells." % TP_S)
+    if index >= TP_S: 
+        ys.exit("error: tape memory out of bounds (overrun)\n"\
+                 "exceeded the tape size of %d cells." % TP_S)
     RPS[TPS[1]] = index
 
-def bi_eval(operator, operand, oper_type):
-    if operator in '~#@$%^!*+-<>:;.=/':
-        if operator in '+-*/^%':
-            eval("MX[TP][RP] %c= operand" % operator)
-        elif operator == '~':
-            if oper_type: tape_num = operand
-            else: return TP
-        elif operator == '#': 
-            if oper_type: RP = operand
-            else: return RP
-        elif operator == '@':
-            MX[other_tape] += operand or MX[TP]
-        elif operator == '$':
-            return MX[TP][operand] or cur_val()
-        elif operator == '!': 
-            MX[TP] += operand or MX[other_tape]
-        elif operator in '<>': RP += (ord(operator) - 61) * operand
-        elif operator == ':':
-            old_RP = RP
-            RP = operand if operand else RP
-            retn = ss_eval(ss_inst(FT[cur_val()]))
-            RP = old_RP
-            return retn
-        elif operator == ';': return operand or cur_val()
-        elif operator == '.': putchar(operand or cur_val())
-        elif operator == '=': sys.exit(operand or cur_val())
+def move(diff):
+    set_RP(RPS[TPS[1]] + diff)
 
-        return None
+def bi_eval(operator, operand, oper_type):
+    """Parse these operators:
+    ~ # @ $ % ^ ! * + < > : ; , . = ? /
+    Bi-parsing  is applicable to these operators.
+    """
+    if operator in '+-*/^%':
+        eval("set_val(get_val() %c operand)" % operator)
+    elif operator == '~':
+        if oper_type:
+            if operand > 7 or operand < 0:
+                sys.exit("Tape number out of range." \
+                         "Requested index: %d" % operand)
+            tape_num = operand
+        else: return TPS[1]
+    elif operator == '#': 
+        if oper_type: set_RP(operand)
+        else: return RPS[TPS[1]]
+    elif operator == '@':
+        MX[RPS[TPS[0]]] += operand or get_val()
+    elif operator == '$':
+        return get_val(operand) or get_val()
+    elif operator == '!': 
+        MX[TP] += operand or MX[other_tape]
+    elif operator in '<>': RP += (ord(operator) - 61) * operand
+    elif operator == ':':
+        old_RP = RP
+        RP = operand if operand else RP
+        retn = ss_eval(FT[get_val()])
+        RP = old_RP
+        return retn
+    elif operator == ';': return operand or get_val()
+    elif operator == '.': putchar(operand or get_val())
+    elif operator == '=': sys.exit(operand or get_val())
+
+    return None
